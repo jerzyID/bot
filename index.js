6114e5b57c6b08e2d48a12730ddf6192aefb9f4d
jerzy <jerzy@united-agency.co.uk>
2017-11-27 15:43:01 +0000

    facebook code part1

--- index.js
+++ index.js
@@ -23,6 +23,7 @@ app.get('/webhook/', function(req, res){
 	res.send ('No entry')	
 })
 
+// Accepts POST requests at /webhook endpoint
 app.post('/webhook', (req, res) => {  
 
   // Parse the request body from the POST
@@ -31,19 +32,27 @@ app.post('/webhook', (req, res) => {
   // Check the webhook event is from a Page subscription
   if (body.object === 'page') {
 
-    // Iterate over each entry - there may be multiple if batched
     body.entry.forEach(function(entry) {
 
-	// Gets the body of the webhook event
-	let webhook_event = entry.messaging[0];
-	console.log(webhook_event);
+      // Gets the body of the webhook event
+      let webhook_event = entry.messaging[0];
+      console.log(webhook_event);
 
-	// Get the sender PSID
-	let sender_psid = webhook_event.sender.id;
-	console.log('Sender PSID: ' + sender_psid);
 
-	});
+      // Get the sender PSID
+      let sender_psid = webhook_event.sender.id;
+      console.log('Sender ID: ' + sender_psid);
 
+      // Check if the event is a message or postback and
+      // pass the event to the appropriate handler function
+      if (webhook_event.message) {
+        handleMessage(sender_psid, webhook_event.message);        
+      } else if (webhook_event.postback) {
+        
+        handlePostback(sender_psid, webhook_event.postback);
+      }
+      
+    });
     // Return a '200 OK' response to all events
     res.status(200).send('EVENT_RECEIVED');
 
